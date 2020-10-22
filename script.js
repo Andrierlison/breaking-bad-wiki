096c54ea0587c4bfef59d4640b55366819019252
Andrierlison <andrierlisonsilva8@gmail.com>
2020-10-22 15:07:20 -0300

    add status to characters

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ function getName() {
     .then((respose) => respose.json())
     .then((data) =>
       data.map((data) => {
-        console.log(data.portrayed);
         let card = document.createElement("div");
-
-        let img = document.createElement("img");
 
         let listName = document.createElement("li");
 
@@ -16,11 +13,19 @@ function getName() {
 
         let listPortrayed = document.createElement("li");
 
+        let listStatus = document.createElement("li");
+
+        let img = document.createElement("img");
+
+        let status = document.createTextNode(`Status: ${data.status}`);
+
         let name = document.createTextNode(`Name: ${data.name}`);
 
         let birthday = document.createTextNode(`Birthday: ${data.birthday}`);
 
         let portrayed = document.createTextNode(`Portrayed: ${data.portrayed}`);
+
+        listStatus.appendChild(status);
 
         listBirthday.appendChild(birthday);
 
@@ -33,11 +38,11 @@ function getName() {
         card.appendChild(listName);
         card.appendChild(listPortrayed);
         card.appendChild(listBirthday);
+        card.appendChild(listStatus);
         card.appendChild(img);
 
         root.appendChild(card);
       })
     );
 }
-
 getName();
